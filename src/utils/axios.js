cf5dfba25fd67a67385b09175e66db326099ed45
yuanwenhui <yuanwenhui93@163.com>
2021-09-09 09:56:33 +0800

    feat: 新建文章

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { message } from "antd";
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:7001',
+  baseURL: 'http://catnofish.cn:7001',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 })
